6b27c885636698fe8d8248f72d72eba6de639058
th3br4vetrave1er <tazdingodan@gmail.com>
2025-01-15 19:31:38 +0200

    exam done

--- main.py
+++ main.py
@@ -1,40 +1,24 @@
 import datetime as dt
-FORMAT = '%Y/%m/%d'
+FORMAT = '%d.%m.%Y'
+lera_bday = dt.date(year=2015, month=5, day=16)
+max_bday = dt.date(year=2011, month=12, day=16)
+birthdays = [
+    ('Лера', '16.05.2015'),
+    ('Максим', '16.12.2011'),
+    ('Толя', '12.06.2016')
+]
 # creating bday function
 
 
 def get_days_to_birthday(name, date_birthday):
-    today = dt.date.today()
+    date_birthday = dt.datetime.strptime(date_birthday, FORMAT)
+    today = dt.datetime.today()
     date_birthday = date_birthday.replace(year=today.year)
     if today > date_birthday:
         date_birthday = date_birthday.replace(year=today+1)
     day_counter = date_birthday - today
-    return day_counter.days
+    return f'{name}, до твоего дня рождения осталось дней: {day_counter.days}'
 
 
-# establish today date
-# today = dt.date.today()
-# print(today)
-
-# establish kids bdays
-lera_bday = dt.date(year=2015, month=5, day=16)
-max_bday = dt.date(year=2011, month=12, day=16)
-
-# establish today year
-# today_year = today.year
-# print(today_year)
-
-# replace kids bdays years on current
-# lera_bday = lera_bday.replace(year=today_year)
-# print(lera_bday)
-# max_bday = max_bday.replace(year=today_year)
-# print(max_bday)
-
-# find difference from today till bdays
-# till_lera_bday = lera_bday - today
-# print(till_lera_bday.days)
-# till_max_bday = max_bday - today
-# print(till_max_bday.days)
-
-print(get_days_to_birthday(lera_bday))
-print(get_days_to_birthday(max_bday))
+for bday in birthdays:
+    print(get_days_to_birthday(bday[0], bday[1]))
